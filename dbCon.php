54e36166422272ba2efd1b6c8bd0c5585aef3a78
Mustafasul <mustafasul.cs2020@gmail.com>
2022-08-26 15:06:23 -0700

    db connection has done...

---
+++ dbCon.php
@@ -0,0 +1,24 @@
+<?php 
+
+  $serverName = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "sama";
+
+
+  $Con = mysqli_connect($serverName, $username, $password, $database);
+  //$con = mysqli_connect("localhost","root","","sama");
+
+  	
+   /*
+
+  	if($Con){
+  		echo "connection has done....";
+  	}
+  	else{
+  		echo "connection failed.";
+  	}
+  	*/
+
+
+?>
